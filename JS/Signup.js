6b8c11f4ad899b19fd721e02a05b9c07b16a6b44
Dahami0629 <136171018+Dahami0629@users.noreply.github.com>
2025-03-14 19:00:06 +0530

    Updates

--- JS/Signup.js
+++ JS/Signup.js
@@ -16,3 +16,9 @@ function showForm(form) {
         loginTab.classList.remove('active');
     }
 }
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    window.location.href = "GamePage.html"; 
+});
